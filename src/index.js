3b707ca6f3f3a7e9efa71a8dabf287d9c5a938c0
Ivelin Dimitrov <ivelin.dimitrov966@gmail.com>
2018-09-25 23:13:13 +0300

    Cleaning unnecessary stuff to start building app.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Hello from './Hello'
 import 'tachyons';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'} />, document.getElementById('root'));
 registerServiceWorker();
